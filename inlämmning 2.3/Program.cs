9d7fa08b58d4c63f41f1b14afb832ed10df91e81
alsv06003 <alsv06003@E-PW0234JW>
2023-09-15 13:38:16 +0200

    Add project files.

---
+++ inlämmning 2.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift_2._3
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("hej hur många dagar ska du hyra en bil");
+            string dagarText=Console.ReadLine();
+            int dagar=int.Parse(dagarText);
+            Console.WriteLine("hur många kilometer ska du köra");
+            string kilometerText=Console.ReadLine();
+            int kilometer=int.Parse(kilometerText);
+            int summa = 300 + (dagar - 1) * 500 + (kilometer * 1);
+            Console.WriteLine("Priset kommer att bli " + summa+ "kr");
+        }
+    }
+}
